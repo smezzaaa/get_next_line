9ff7570668b63472cfbc22c196ba639eee523555
sofia <sofia.mezar06@gmail.com>
2026-01-22 18:12:31 +0100

    add gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,40 @@
 /*   By: smeza-ro <smeza-ro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 12:28:22 by smeza-ro          #+#    #+#             */
-/*   Updated: 2026/01/21 20:00:29 by smeza-ro         ###   ########.fr       */
+/*   Updated: 2026/01/22 18:12:08 by smeza-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+/* 
 static char *ft_stash(ssize_t nb, void *buff)
 {
 	void	*stash;
 	int		i;
-	
-	stash = malloc(nb)
-}
+
+	stash = malloc(nb * sizeof(char));
+} */
 
 char *get_next_line(int fd)
 {
-	void	*buff;
+	void		*buff;
 	ssize_t		rd;
+	static char	*stash = NULL;
 
-	buff = malloc(BUFFER_SIZE * sizeof(char));
+	rd = 1;
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return(NULL);
-	while (rd != 0)
+	while (rd > 0 && !ft_strchr(stash, '\n'))
 	{
 		rd = read(fd, buff, BUFFER_SIZE);
-		ft_stash(rd, buff);
+		if (!stash)
+			stash = ft_strdup(buff);
+		else
+		{
+			temp = ft_strjoin(stash, buff);
+			free (stash);
+		}
 	}
 }
 
